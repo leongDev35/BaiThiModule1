75dd1e0ea0d79c9bb90edf1bda6d8e27d48e3dc0
unknown <leong.dev.35@gmail.com>
2023-02-07 15:21:46 +0700

    2nd

--- cau3.js
+++ cau3.js
@@ -1,11 +1,36 @@
-function sumOfListPrime(x) {
-    let tong = 0;
-    let cong = 0;
-    while(cong<=x) {
-        tong += cong ;
-        cong++;
+function kiem_tra_snt(n)
+{
+    // Biến cờ hiệu
+    var flag = true;
+ 
+    // Nếu n bé hơn 2 tức là không phải số nguyên tố
+    if (n < 2){
+        flag = false;
     }
-    return tong;
+    else{
+        // lặp từ 2 tới n-1
+        for (var i = 2; i < n-1; i++)
+        {
+            if (n % i == 0){
+                flag = false;
+                break;
+            }
+        }
+    }
+ 
+    return flag;
 }
-let x = -5;
+function sumOfListPrime(x) {
+    let tongSoNguyenTo = 0;
+    let soNguyenTo = 0;
+    while(soNguyenTo<=x) {
+        if(kiem_tra_snt(soNguyenTo)){
+
+            tongSoNguyenTo += soNguyenTo ;
+        }
+        soNguyenTo++;
+    }
+    return tongSoNguyenTo;
+}
+let x = 10;
 console.log(sumOfListPrime(x));
